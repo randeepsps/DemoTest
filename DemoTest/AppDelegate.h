bb09f1e1cb5eec7cac76414f4ac631d77049bbc3
Sanjeevareddy Nandela <sanjeevareddy@Sanjeev-Mac-Mini.local>
2015-08-06 16:54:47 +0530

    Initial Commit

---
+++ DemoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoTest
+//
+//  Created by Sanjeevareddy Nandela on 8/6/15.
+//  Copyright (c) 2015 Sanjeevareddy Nandela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
